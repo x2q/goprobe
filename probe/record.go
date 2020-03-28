e953b4df7f8589cb846d0b1dce1a3a25ddd29047
x2q <x2q@x2q.net>
2020-03-28 23:16:39 +0100

    adds SSID

--- probe/record.go
+++ probe/record.go
@@ -11,6 +11,7 @@ type ProbeRecord struct {
 	Timestamp  int64        `json:"timestamp"`
 	Mac        string       `json:"mac"`
 	SequenceId int          `json:"sequence_id"`
+	SsId       string       `json:"ssid"`
 	Rssi       int          `json:"rssi"`
 	ApName     string       `json:"ap_name,omitempty"`
 }
@@ -19,6 +20,7 @@ func (r *ProbeRecord) Values() (values url.Values) {
 	values.Add("timestamp", strconv.FormatInt(r.Timestamp, 10))
 	values.Add("mac", r.Mac)
 	values.Add("sequence_id", strconv.Itoa(r.SequenceId))
+	values.Add("ssid", r.SsId)
 	values.Add("rssi", strconv.Itoa(r.Rssi))
 	if r.ApName != "" {
 		values.Add("ap_name", r.ApName)
@@ -29,7 +31,7 @@ func (r *ProbeRecord) Values() (values url.Values) {
 func (r *ProbeRecord) String() string {
 	ltime := time.Unix(r.Timestamp, 0).Local()
 	if r.ApName != "" {
-		return fmt.Sprintf("%s,%d,%s,%d,%d,%s", ltime.Format(time.RFC3339), r.Timestamp, r.Mac, r.SequenceId, r.Rssi, r.ApName)
+		return fmt.Sprintf("%s,%d,%s,%d,%s,%d,%s", ltime.Format(time.RFC3339), r.Timestamp, r.Mac, r.SequenceId, r.SsId, r.Rssi, r.ApName)
 	}
-	return fmt.Sprintf("%s,%d,%s,%d,%d", ltime.Format(time.RFC3339), r.Timestamp, r.Mac, r.SequenceId, r.Rssi)
+	return fmt.Sprintf("%s,%d,%s,%d,%s,%d", ltime.Format(time.RFC3339), r.Timestamp, r.Mac, r.SequenceId, r.SsId, r.Rssi)
 }
